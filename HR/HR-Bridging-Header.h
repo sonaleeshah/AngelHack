9d79037e605e81bf6e50231ad5bf34feef00e017
sonaleeshah <sonaleeshah@Sonalees-MacBook-Pro-2.local>
2016-06-11 19:40:58 -0400

    UI

---
+++ HR/HR-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "RPFloatingPlaceholderConstants.h"
+#import "RPFloatingPlaceholderTextField.h"
+#import "RPFloatingPlaceholderTextView.h"
---
+++ HR/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HR
+//
+//  Created by Sonalee Shah on 2016-06-11.
+//  Copyright © 2016 Alexathon. All rights reserved.
+//
+
+import UIKit
+//import RPFloatingPlaceholderTextField
+//import RPFloatingPlaceholderTextView
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
